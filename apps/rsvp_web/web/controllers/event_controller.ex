6960b81aa3f6780a14f654c7f5439c717ca9bd1b
Edgar Pino <me@edgarpino.com>
2017-07-14 13:35:38 -0500

    added event templates

--- apps/rsvp_web/web/controllers/event_controller.ex
+++ apps/rsvp_web/web/controllers/event_controller.ex
@@ -4,9 +4,12 @@ defmodule RsvpWeb.EventController do
 
     def show(conn, %{"id" => id} = params ) do
         event = Rsvp.EventQueries.get_by_id(id)
-        |> IO.inspect
-
         render conn, "details.html", event: event
+    end
+
+    def list(conn, _params) do
+        events = Rsvp.EventQueries.get_all()
+        render conn, "list.html", events: events
     end
 
 
