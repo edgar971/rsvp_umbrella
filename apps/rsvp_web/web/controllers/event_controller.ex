0911ae67d1e0fc4e7ffdceb4ca3277bcff855e17
Edgar Pino <me@edgarpino.com>
2017-07-14 14:04:05 -0500

    Added form to add event

--- apps/rsvp_web/web/controllers/event_controller.ex
+++ apps/rsvp_web/web/controllers/event_controller.ex
@@ -2,7 +2,7 @@ defmodule RsvpWeb.EventController do
     
     use RsvpWeb.Web, :controller
 
-    def show(conn, %{"id" => id} = params ) do
+    def show(conn, %{"id" => id}) do
         event = Rsvp.EventQueries.get_by_id(id)
         render conn, "details.html", event: event
     end
@@ -12,5 +12,17 @@ defmodule RsvpWeb.EventController do
         render conn, "list.html", events: events
     end
 
+    def create(conn, _params) do
+        changeset = Rsvp.Event.changeset(%Rsvp.Event{}, %{})
+        render conn, "create.html", changeset: changeset
+    end
+
+    def add(conn, %{"event" => event}) do
+        event = Map.update!(event, "date", fn d -> d <> ":00" end)
+        %{id: id} = Rsvp.Event.changeset(%Rsvp.Event{}, event)
+        |>  Rsvp.EventQueries.create
+        
+        redirect conn, to: event_path(conn, :show, id)
+    end
 
 end
